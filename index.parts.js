7cf5916cc889b8f23ed665aecaecf1f177ca1f3f
Dave England <DavidEngland@hotmail.com>
2016-10-15 12:58:57 -0500

    mods

--- index.parts.js
+++ index.parts.js
@@ -1,4 +1,5 @@
 var fs = require("fs");
+var path = require('path');
 var Handlebars = require("handlebars");
 var marked = require("marked");
 
@@ -9,6 +10,20 @@ module.exports = {
 function render(resume) {
     var css = fs.readFileSync(__dirname + "/style.css", "utf-8");
     var template = fs.readFileSync(__dirname + "/resume.hbs", "utf-8");
+    var partialsDir = path.join(__dirname, 'partials');
+    var filenames = fs.readdirSync(partialsDir);
+
+    filenames.forEach(function(filename) {
+        var matches = /^([^.]+).hbs$/.exec(filename);
+        if (!matches) {
+            return;
+        }
+        var name = matches[1];
+        var filepath = path.join(partialsDir, filename)
+        var template = fs.readFileSync(filepath, 'utf8');
+
+        Handlebars.registerPartial(name, template);
+    });
 
     return Handlebars.compile(template)({
         css: css,
